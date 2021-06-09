076c2bf2b96f17442566cb96c28b8054dea9d6a2
DimonNester <dimon.nester21@mail.ru>
2021-06-09 16:51:45 +0300

    zhunt hist has been added

--- src/len_hist.R
+++ src/len_hist.R
@@ -1,17 +1,18 @@
 
-source('lib.R')
+source("lib.R")
 
 ###
-
+NAME <- 'zhunt'
 #NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
 #NAME <- 'H3K4me3_H1.ENCFF408FCY.hg38'
 #NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
-NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
+#NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
 
 ###
 
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
