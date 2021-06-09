05ab9e3b35d0d90ce7852fadb8a6e8ead935c152
DimonNester <dimon.nester21@mail.ru>
2021-06-09 15:31:01 +0300

    Visualisation has been added

---
+++ src/chip_seeker.R
@@ -0,0 +1,45 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene", force = TRUE)
+# BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+
+BiocManager::install("clusterProfiler", force = TRUE)
+BiocManager::install("ChIPseeker", force = TRUE)
+BiocManager::install("org.Hs.eg.db", force = TRUE)
+BiocManager::install("ChIPpeakAnno", force = TRUE)
+
+source("lib.R")
+
+###
+
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+library(ChIPpeakAnno)
+library(org.Hs.eg.db)
+
+###
+
+#NAME <- 'H3K4me3_A549.intersect_with_DeepZ'
+#NAME <- 'DeepZ'
+NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19.filtered'
+#NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+ peak <- readPeakFile(BED_FN)
+ pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+ covplot(peak, weightCol="V5")
+ dev.off()
+# 
--- src/filter_peaks.R
+++ src/filter_peaks.R
@@ -3,10 +3,9 @@ source('lib.R')
 
 ###
 
-#NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
-#NAME <- 'H3K4me3_H1.ENCFF408FCY.hg38'
-#NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
-NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
+
+# NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
+ NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
 
 ###
 
@@ -15,28 +14,16 @@ colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
-# hist(bed_df$len)
-
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
 
-###########
-# NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
-NAME <- 'H3K4me3_H1.ENCFF408FCY.hg38'
-# NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
-# NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
-OUT_DIR <- 'Results/'
 
-###
+####
 
-bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
-bed_df$len <- bed_df$end - bed_df$start
-head(bed_df)
 
 bed_df <- bed_df %>%
   arrange(-len) %>%
@@ -47,11 +34,11 @@ ggplot(bed_df) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
 
 bed_df %>%
   select(-len) %>%
-  write.table(file='data/H3K4me3_H1.ENCFF408FCY.hg38.filtered.bed',
+  write.table(file=paste0(DATA_DIR, NAME, '.filtered.bed'),
               col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
 
 
