71f6d1c7a84c654c5b4b960e788d88a6c40c100d
DimonNester <dimon.nester21@mail.ru>
2021-06-09 11:52:29 +0300

    lib.R, len_hist.R, and filter_peaks.R with its results were added

---
+++ src/filter_peaks.R
@@ -0,0 +1,57 @@
+
+source('lib.R')
+
+###
+
+#NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
+#NAME <- 'H3K4me3_H1.ENCFF408FCY.hg38'
+#NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
+NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+# hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
+
+###########
+# NAME <- 'H3K4me3_H1.ENCFF408FCY.hg19'
+NAME <- 'H3K4me3_H1.ENCFF408FCY.hg38'
+# NAME <- 'H3K4me3_H1.ENCFF668YOE.hg19'
+# NAME <- 'H3K4me3_H1.ENCFF668YOE.hg38'
+OUT_DIR <- 'Results/'
+
+###
+
+bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 5000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file='data/H3K4me3_H1.ENCFF408FCY.hg38.filtered.bed',
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+
+
---
+++ src/lib.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(dplyr)
+
+###
+
+DATA_DIR <- '../data/'
+OUT_DIR <- '../images/'
